e84d13382dfc66a9806981ec626096e9487b097f
Jari Himanen <jari.himanen@tietoevry.com>
2023-10-25 07:44:41 +0300

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
 ﻿
+using API.Extensions;
+
 namespace API.Entities
 {
 
@@ -12,6 +14,31 @@ namespace API.Entities
 
         public byte[] PasswordSalt { get; set; }
 
-    }
+        public DateOnly DateOfBirth { get; set; }
 
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+        public string Gender { get; set; }
+
+        public string Intoduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public List<Photo> Photos { get; set; } = new();
+
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();            
+        // }
+    }
 }
