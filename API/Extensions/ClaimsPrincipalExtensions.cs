de9d5f635928dee810c19d3dcfc19c243052cdd3
Jari Himanen <jari.himanen@tietoevry.com>
2024-02-26 07:49:31 +0200

    End of section 13

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -7,8 +7,13 @@ namespace API.Extensions
         public static string GetUsername(this ClaimsPrincipal user)
         {
             // Get the username from the token
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
         }
 
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            // Get the user id from the token
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
     }
 }
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -20,7 +20,8 @@ namespace API.Services
         {
             var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                new (JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new (JwtRegisteredClaimNames.UniqueName, user.UserName)
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
