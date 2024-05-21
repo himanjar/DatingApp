24256cc8d21ce96450495453053ab19ec607b12f
Jari Himanen <jari.himanen@tietoevry.com>
2024-05-21 08:06:47 +0300

    End of section 16

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
@@ -10,13 +11,15 @@ namespace API.Services
     public class TokenService : ITokenService
     {
         private readonly SymmetricSecurityKey _key;
+        private readonly UserManager<AppUser> _userManager;
 
-        public TokenService(IConfiguration config)
+        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+            _userManager = userManager;
         }
 
-        public string CreateToken(AppUser user)
+        public async Task<string> CreateToken(AppUser user)
         {
             var claims = new List<Claim>
             {
@@ -24,7 +27,11 @@ namespace API.Services
                 new (JwtRegisteredClaimNames.UniqueName, user.UserName)
             };
 
+            var roles = await _userManager.GetRolesAsync(user);
+
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
