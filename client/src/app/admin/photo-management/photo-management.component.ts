7957aea44f3a39138d195577180fa14a2be11a28
himanjar <40797094+himanjar@users.noreply.github.com>
2024-09-27 08:35:27 +0300

    Client: Changes to photo-editor component and admin services

--- client/src/app/admin/photo-management/photo-management.component.ts
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Photo } from 'src/app/_models/photo';
+import { AdminService } from 'src/app/_services/admin.service';
 
 @Component({
   selector: 'app-photo-management',
   templateUrl: './photo-management.component.html',
   styleUrls: ['./photo-management.component.css']
 })
-export class PhotoManagementComponent {
+export class PhotoManagementComponent implements OnInit {
+  photos: Photo[] = [];
 
+  constructor(private adminService: AdminService) { }
+  
+  ngOnInit(): void {
+    this.getPhotosForApproval();
+  }
+
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe({
+      next: photos => this.photos = photos
+    })   
+  }
 }
